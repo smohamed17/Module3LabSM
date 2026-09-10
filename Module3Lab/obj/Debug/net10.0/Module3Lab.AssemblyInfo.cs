266a3824e78348589b57c67f2f417de7a714e431
Said Mohamed <sai7mohaa@gmail.com>
2026-09-10 00:22:52 +0000

    Updatining commit

--- Module3Lab/obj/Debug/net10.0/Module3Lab.AssemblyInfo.cs
+++ Module3Lab/obj/Debug/net10.0/Module3Lab.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module3Lab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+117b7073ca09a49badcafedf12e558b269f1240f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88479f7ce0c04650aef582f088654e08b3e91b51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module3Lab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module3Lab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
